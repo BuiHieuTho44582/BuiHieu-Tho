033774aad13aec87b1c48e62e40c895283cda8b8
BuiHieuTho44582 <90257715+BuiHieuTho44582@users.noreply.github.com>
2021-11-03 17:10:15 +0700

    Add files via upload

---
+++ BuiHieuTho_44582_CH03/excercise/page_85_exercise_03.py
@@ -0,0 +1,13 @@
+"""
+Author: Bùi hiếu thọ
+Date: 18/09/2021
+
+Problem:   Assume that x refers to a number. Write a code segment that prints the number’s
+absolute value without using Python’s abs function.
+
+Solution:
+
+    ....
+"""
+string = input("Nhập string: ")
+print(string.count(' '))
---
+++ BuiHieuTho_44582_CH03/project/page_99_project_01.py
@@ -0,0 +1,19 @@
+"""
+Author: Bùi hiếu thọ
+Date: 18/09/2021
+Problem:    Write a program that accepts the lengths of three sides of a triangle as inputs.
+The program output should indicate whether or not the triangle is an equilateral
+triangle.
+
+Solution:
+
+    ....
+"""
+canh1 = int(input("Nhập cạnh thứ nhất:"))
+canh2 = int(input("Nhập cạnh thứ hai:"))
+canh3 = int(input("Nhập cạnh thứ ba:"))
+print("")
+if canh1 == canh2 == canh3:
+    print("Là tam giác đều")
+else:
+    print("Không là tam giác đều")
